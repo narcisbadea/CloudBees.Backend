557fd2fc62af22c20677e54d785b0052868beb5b
Narcis Badea <badeanarcis@gmail.ocm>
2022-12-03 18:16:30 +0200

    Added firstName and lastName at register

--- CloudBees.BLL/DTOs/RegisterDTO.cs
+++ CloudBees.BLL/DTOs/RegisterDTO.cs
@@ -10,4 +10,7 @@ public class RegisterDTO
 {
     public string  Email { get; set; } = String.Empty;
     public string Password { get; set; } = String.Empty;
+    public string FirstName { get; set; } = String.Empty;
+    public string LastName { get; set; } = String.Empty;
+
 }
--- CloudBees.DAL/Entities/User.cs
+++ CloudBees.DAL/Entities/User.cs
@@ -5,5 +5,7 @@ namespace CloudBees.DAL.Entities;
 public class User : IdentityUser
 {
     public int Points { get; set; }
+    public string FirstName { get; set; } = String.Empty;
+    public string LastName { get; set; } = String.Empty;
 }
 
